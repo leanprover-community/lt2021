7c73bb9a3dd7171bc24c2761d6f3c28bdb6a620e
Patrick Massot <patrickmassot@free.fr>
2020-12-13 22:00:44 +0100

    Different local time display

--- assets/js/dates.js
+++ assets/js/dates.js
@@ -1,10 +1,9 @@
 $(function() {
-$('time').each(function() { 
-    if (Intl.DateTimeFormat().resolvedOptions().timeZone == 'Europe/Paris') {
-	    $(this).siblings('span').hide()
-    };
-    var dt = new Date($(this).attr('datetime'));
-    var m = dt.getMinutes();
-    $(this).html(dt.getHours() + ':' + m.toString().padStart(2, '0'));
-})
+    var tz = Intl.DateTimeFormat().resolvedOptions().timeZone
+
+		$('time').each(function() { 
+				var dt = new Date($(this).attr('datetime'));
+				var m = dt.getMinutes();
+				$(this).html(dt.getHours() + ':' + m.toString().padStart(2, '0') + ' ('+tz+' time)');
+		})
 });
